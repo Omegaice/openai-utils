29f4c0d3f181c8cb15e7e699d0f2fa52a20055da
Omegaice <950526+Omegaice@users.noreply.github.com>
2025-04-23 20:38:35 -0400

    feat: expose classes from base package

--- src/openai_utils/__init__.py
+++ src/openai_utils/__init__.py
@@ -1,2 +1,4 @@
-def hello() -> str:
-    return "Hello from openai-utils!"
+from openai_utils.conversation import Conversation, ConversationManager
+from openai_utils.models import Model
+
+__all__ = ["Conversation", "ConversationManager", "Model"]
